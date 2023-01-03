b40d126fcdd88ff767eb4bc65be559b4e4ad819f
annayakymova <95213073+annayakymova@users.noreply.github.com>
2023-01-03 15:49:22 +0200

    Update README.md

--- app.py
+++ app.py
@@ -1,19 +1,17 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-
-
 app = Flask(__name__)
 app.config.from_object("config.Config")
 db = SQLAlchemy(app)
-db.init_app(app)
 
 
+# db.init_app(app)
 with app.app_context():
-    from routes.main import *
-    from routes.plants import *
-    from routes.employee import *
-    from models.models import *
+    from routes import *
+    from models.models import Plant, Employee
+
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
--- routes/plants.py
+++ routes/plants.py
@@ -1,19 +1,25 @@
 from app import app, db
 from flask import render_template, request, redirect
-from models.models import Plant
+from models.models import Plant, Employee
 
 
 @app.route("/add-plant")
 def add_plant():
-    return render_template("add_plant.html")
+    employees = Employee.query.all()
+    return render_template("add_plant.html", employees=employees)
 
 
 @app.route("/save-plant", methods=["POST"])
 def save_plant():
+    # print(request.form.getlist("employees"))
     name = request.form.get("name")
     location = request.form.get("location")
     plant = Plant(title=name, location=location)
     db.session.add(plant)
+    for employee_id in request.form.getlist("employees"):
+        employee = Employee.query.get(int(employee_id))
+        employee.plant_id = plant.id
+        db.session.add(employee)
     db.session.commit()
     return redirect("/")
 
@@ -21,13 +27,28 @@ def save_plant():
 @app.route("/delete-plant/<int:id>")
 def delete_plant(id):
     plant = Plant.query.get(id)
+    print(plant.id)
     db.session.delete(plant)
     db.session.commit()
     return redirect("/")
 
 
-@app.route("/plant-info/<int:id>")
-def info_plant(id):
-    info_plant = Plant.query.get(id)
-    print(Plant.title)
-    return render_template("plant_info.html", info_plant=info_plant)
+@app.route("/edit-plant/<int:id>")
+def edit_plant(id):
+    plant = Plant.query.get(id)
+    employees = Employee.query.all()
+    return render_template("add_plant.html", plant=plant, employees=employees)
+
+
+@app.route("/update-plant/<int:id>", methods=["POST"])
+def update_plant(id):
+    plant = Plant.query.get(id)
+    plant.title = request.form.get("name")
+    plant.location = request.form.get("location")
+    db.session.add(plant)
+    for employee_id in request.form.getlist("employees"):
+        employee = Employee.query.get(int(employee_id))
+        employee.plant_id = plant.id
+        db.session.add(employee)
+    db.session.commit()
+    return redirect("/")
